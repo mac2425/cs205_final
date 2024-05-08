75684c1d5c8f521e52a8c90b9d670855a90fbeef
mac2425 <128522345+mac2425@users.noreply.github.com>
2024-05-07 21:29:23 -0700

    Create cs205_final_exam.sh

---
+++ cs205_final_exam.sh
@@ -0,0 +1,13 @@
+# TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like best_pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
+# ===== SUMMARY OF DATA FILE =====
+#    File name: best_pokemon.dat
+#    Total Pokemon: 703
+#    Avg. HP: 224.697
+#    Avg. Attack: 108.153
+# ===== END SUMMARY =====
+
+# The "Avg." values should be calculated as mean values for the corresponding columns.
+# The spacing and header formatting should match the above formatting description exactly.
+# There should be a comment explaining the purpose of each line in your shell script. 
+# The data file will be passed in to the script as a positional parameter and will not necessarily be called best_pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way best_pokemon.dat is formatted.
+
