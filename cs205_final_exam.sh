df0d8fc98fb49815d7fe5f00167f94abeb1b0605
mac2425 <128522345+mac2425@users.noreply.github.com>
2024-05-07 21:52:36 -0700

    Update cs205_final_exam.sh

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,9 +1,9 @@
 # TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like best_pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
 # ===== SUMMARY OF DATA FILE =====
-#    File name: [VALUE]
-#    Total Pokemon: [VALUE]
-#    Avg. HP: [VALUE]
-#    Avg. Attack: [VALUE]
+#    File name: best_pokemon.dat
+#    Total Pokemon: 703
+#    Avg. HP: 224.697
+#    Avg. Attack: 108.153
 # ===== END SUMMARY =====
 
 # The "Avg." values should be calculated as mean values for the corresponding columns.
